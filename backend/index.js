8283d44e79d7c77b337ab58147a35c03f724e354
asdf99245 <asdf99245@naver.com>
2023-04-02 23:41:52 +0900

    chore: eslint 수정

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
 const app = (0, express_1.default)();
 const port = 5000;
 app.get('/', (req, res) => {
-    res.send('Typescript + Node.js + Express Server');
+  res.send('Typescript + Node.js + Express Server');
 });
 app.listen(port, () => {
-    console.log(`[server]: Server is running at <http://localhost>:${port}`);
+  console.log(`[server]: Server is running at <http://localhost>:${port}`);
 });
