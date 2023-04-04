a60f6af6846630efae465a2940b228b6b46f3f8f
tunggary <gsy1656@gmail.com>
2023-04-04 10:10:30 +0900

    chore: sass setting
    Co-authored-by: Yunkyu Jung <asdf99245@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ['./src/styles'],
+        additionalData: `
+              @import "_colors.scss";
+            `,
+      },
+    },
+  },
 });
