2d18b01cf06cd2c8c7bdca4f6ba102a5d56df2b2
tunggary <gsy1656@gmail.com>
2023-05-05 17:18:50 +0900

    feat: message type, chatting input type 추가
    Co-authored-by: Yunkyu Jung <asdf99245@users.noreply.github.com>

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,1 +1,3 @@
 export type ButtonType = 'arrow-left' | 'arrow-right' | 'close' | 'hexagon' | 'square' | 'rectangle';
+export type MessageType = 'normal' | 'private' | 'group';
+export type ChattingInputType = 'all' | 'group' | 'private';
