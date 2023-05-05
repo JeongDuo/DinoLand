471949697597d3f7a0811fd3d0f4a67af6e9c9b4
tunggary <gsy1656@gmail.com>
2023-05-05 17:18:21 +0900

    feat: chatting input type 상수 추가
    Co-authored-by: Yunkyu Jung <asdf99245@users.noreply.github.com>

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,1 +1,9 @@
+import { ChattingInputType } from '@/types';
+
 export const ASSETS_PATH = 'src/assets/';
+
+export const CHATTING_INPUT_TYPE: Record<ChattingInputType, string> = {
+  all: '전체',
+  group: '그룹',
+  private: '귓속말',
+} as const;
