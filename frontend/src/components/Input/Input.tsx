b1019d429ed1aab38e8e2295434f49dc0dd01b17
tunggary <gsy1656@gmail.com>
2023-05-13 00:15:23 +0900

    feat: Input 컴포넌트 className props 추가
    Co-authored-by: Yunkyu Jung <asdf99245@users.noreply.github.com>

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -3,11 +3,20 @@ import styles from './Input.module.scss';
 interface Props {
   value: string;
   placeholder?: string;
+  className?: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-function Input({ value, placeholder = '이름을 입력하세요', onChange }: Props) {
-  return <input className={styles.input} type='text' value={value} placeholder={placeholder} onChange={onChange} />;
+function Input({ value, placeholder = '이름을 입력하세요', className, onChange }: Props) {
+  return (
+    <input
+      className={`${styles.input} ${className}`}
+      type='text'
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
 }
 
 export default Input;
