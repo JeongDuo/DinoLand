eb191a0ced7fb5ccefeec28dba9b2c45ef687d19
tunggary <gsy1656@gmail.com>
2023-06-01 22:54:51 +0900

    refactor: 정적 파일 경로 변경
    Co-authored-by: Yunkyu Jung <asdf99245@users.noreply.github.com>

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,9 +1,16 @@
 import { ChattingInputType } from '@/types';
 
-export const ASSETS_PATH = 'src/assets/';
-
+export const ASSETS_PATH = '';
 export const PLACEHOLDER_INPUT_DEFAULT = '메시지를 입력하세요.';
 export const PLACEHOLDER_INPUT_PRIVATE = 'Ex) "@사용자명 메시지" 형식으로 입력하세요.';
+
+export const MODAL_ASSETS = [
+  '/assets/dinoSprites-mort.png',
+  '/assets/close.png',
+  '/assets/modal.png',
+  '/assets/arrow-left.png',
+  '/assets/arrow-right.png',
+];
 
 export const CHATTING_INPUT_TYPE: Record<ChattingInputType, string> = {
   all: '전체',
